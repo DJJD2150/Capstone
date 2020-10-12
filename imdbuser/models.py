a7e4375e4aee6994480d932989a1162e6ed0787a
Leanne Benson <stylistleannerowe@gmail.com>
2020-10-12 15:23:13 -0400

    reviews app added lots of rubric points. Secret key function added and gitignore file

--- imdbuser/models.py
+++ imdbuser/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
+#put date user signed up here for final model 
 
 class BaseUser(AbstractUser):
     name = models.CharField(blank=True, null=True, max_length=25)
